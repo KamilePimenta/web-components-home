0389ec4573640dd9a398ecc27e0936844c8cb0b0
Kamile Pimenta <kamile.pimenta@outlook.com>
2024-10-27 16:01:14 -0300

    feat: prepare custom web component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // entryComponents: [AppComponent]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+  constructor( private injector: Injector) {}
+  ngDoBootstrap() {
+    const childAppComponent =
+        createCustomElement(AppComponent, {
+           injector: this.injector
+        });
+    customElements.define('kms-home', childAppComponent);
+  }
+}
