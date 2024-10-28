15d9978027e260537173c04ca5a81f0ed2633b1c
Kamile Pimenta <kamile.pimenta@outlook.com>
2024-10-28 16:11:56 -0300

    feat: remove ngx-build-plus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,11 +20,9 @@ import { HomeComponent } from './pages/home/home.component';
 })
 export class AppModule {
   constructor( private injector: Injector) {}
+
   ngDoBootstrap() {
-    const childAppComponent =
-        createCustomElement(AppComponent, {
-           injector: this.injector
-        });
+    const childAppComponent = createCustomElement(AppComponent, { injector: this.injector });
     customElements.define('kms-home', childAppComponent);
   }
 }
